aa8446ce2f5d298dd265cdb50d5a0ac0c30cad81
githubuiux <uiuxista@gmail.com>
2023-06-26 16:54:56 +0300

    Initial Commit

---
+++ GlassBackground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GlassBackground
+//
+//  Created by Levent on 26.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GlassBackground/GlassBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassBackgroundApp.swift
+//  GlassBackground
+//
+//  Created by Levent on 26.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GlassBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
